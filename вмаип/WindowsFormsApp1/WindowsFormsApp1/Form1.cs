cfa8acba145481ac3b90ec9f3dfffc1204271136
Vitaliy <vitaliy.sukhinskiy@mail.ru>
2024-09-14 13:01:22 +0400

    Add files via upload

---
+++ вмаип/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+                {
+                pictureBox1.Hide();
+                pictureBox2.Show();
+            }
+            else {
+              pictureBox1.Show();
+                pictureBox2.Hide();
+            }
+        }
+    }
+}
